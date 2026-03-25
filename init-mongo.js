67449352b0c8b14830b7fb376f15b7fb4e7ea4c1
Alberto736 <alberto.guerra@b2bit.es>
2026-03-25 10:14:38 +0000

    fix: Remove hermod reference from init-mongo.js
    - Update MongoDB init script to use security_api database
    - Change user role from hermod to security_api
    - Update comments and print statements
    - Last hermod reference eliminated

--- init-mongo.js
+++ init-mongo.js
@@ -1,12 +1,12 @@
 // MongoDB initialization script
-// Creates application user for the hermod database
-db = db.getSiblingDB("hermod");
+// Creates application user for the security_api database
+db = db.getSiblingDB("security_api");
 
 db.createUser({
   user: "user",
   pwd: "change_this_password_in_production",
-  roles: [{ role: "readWrite", db: "hermod" }],
+  roles: [{ role: "readWrite", db: "security_api" }],
 });
 
-print("Application user created for hermod database");
+print("Application user created for security_api database");
 
